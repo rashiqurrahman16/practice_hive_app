4a8208b49e1cdc931efbaf3c87ae0b3e5be7299b
rashiqurrahman16 <rashiqur.rahman16@gmail.com>
2024-01-30 14:59:12 +0600

    learned about box

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -14,11 +15,73 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Hive App"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
       ),
       body: Column(
         children: [
+          FutureBuilder(
+            future: Hive.openBox('oyon'),
+            builder: (context, snapshot){
+                return Column(
+                  children: [
+                    ListTile(
+                      title: Text(snapshot.data!.get('name').toString()),
+                      subtitle: Text(snapshot.data!.get('age').toString()),
+                    ),
+                    Text(snapshot.data!.get('name').toString()),
+                    Text(snapshot.data!.get('age').toString()),
+                    Text(snapshot.data!.get('details').toString()),
+                  ],
+                );
+              },
+          ),
+          FutureBuilder(
+            future: Hive.openBox('name'),
+            builder: (context, snapshot){
+              return Column(
+                children: [
+                  ListTile(
+                    title: Text(snapshot.data!.get('abc').toString()),
+                    trailing: IconButton(
+                      onPressed: (){
+                        snapshot.data!.put('abc', 'Rashiqur r');   //if i press the edit button then previous name will be edited with this one
+                        setState(() {
+                          
+                        });
+                      }, icon: Icon(Icons.edit),
+                    ),
+
+                  ),
+                ],
+              );
+            },
+          ),
 
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            var box = await Hive.openBox('oyon');
+            var box2 = await Hive.openBox('name');
+
+
+            box2.put('abc', 'Rashiqur Rahman');
+            box.put('name', 'RR oyon');
+            box.put('age', '23');
+
+            box.put('details', {
+              'pro' : 'developer',
+              'a' : 'aaaa'
+            });
+
+            print(box.get('name'));
+            print(box.get('age'));
+            print(box.get('details'));
+            print(box.get('details')['pro']);
+
+          },
+        child:  const Icon(Icons.add),
       ),
     );
   }
